2dedeea11389643d4d5df3258fabc83b965649a3
edhernandez04 <mchh.004@gmail.com>
2020-02-25 15:41:12 -0500

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -5,14 +5,8 @@ const POKEMONS_URL = `${BASE_URL}/pokemons`
 Board = document.getElementsByTagName('main')[0]
 
 document.addEventListener('DOMContentLoaded', () => {
-
     fetch(TRAINERS_URL).then(resp => resp.json()).then(trainers => displayTrainers(trainers))
-
 })
-    const trainersPokemon = pokemon => {
-        return `<li>${pokemon.nickname}(${pokemon.species})
-        <button class="release" id="${pokemon.id}">Release</button></li>`
-    }
 
     function displayTrainers(trainers){
         for (let i=0; i < trainers.length; i++){
@@ -22,39 +16,47 @@ document.addEventListener('DOMContentLoaded', () => {
             tCard.innerHTML = `
                 <p>${trainers[i].name}</p>
                 <button class="add" id="${trainers[i].id}"> Add Pokemon </button>
-                <ul>${trainers[i].pokemons.map(trainersPokemon).join("")}
-                </ul>
-                `
+                <ul>${trainers[i].pokemons.map(trainersPokemon).join("")}</ul>`
             Board.append(tCard)
-
         }      
+    }
+
+    const trainersPokemon = pokemon => {
+        return `<li>${pokemon.nickname}(${pokemon.species})
+        <button class="release" id="${pokemon.id}">Release</button></li>`
+    }
+
+    function addPokemon(e){
+        fetch(`${POKEMONS_URL}`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                "trainer_id": e.target.id
+            })
+        }).then(resp => resp.json()).then(pokemon => {
+                e.target.parentElement.children[2].innerHTML += `<li>${pokemon.nickname}(${pokemon.species})
+                <button class="release" id="${pokemon.id}">Release</button></li>`
+            })
+    }
+
+    function deletePokemon(e){
+        fetch(`${POKEMONS_URL}/${e.target.id}`, {
+            method: "DELETE"
+        }).then(resp => resp.json())
+        .then(
+            e.target.parentNode.remove()
+        )
     }
 
     document.addEventListener('click', function(e){
         if (e.target.className === "release"){
-            fetch(`${POKEMONS_URL}/${e.target.id}`, {
-                method: "DELETE"
-            }).then(resp => resp.json())
-            .then(
-                e.target.parentNode.remove()
-            )
+            deletePokemon(e)
         } else if (e.target.className === "add") {
             if (e.target.parentNode.children[2].childElementCount >= 6) { alert("Cannot have more than 6 Pokemon!")
-            }else{
-                fetch(`${POKEMONS_URL}`, {
-                    method: "POST",
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({
-                        "trainer_id": e.target.id
-                    })
-                }).then(resp => resp.json())
-                    .then(pokemon => {
-                        e.target.parentElement.children[2].innerHTML += `<li>${pokemon.nickname}(${pokemon.species})
-                        <button class="release" id="${pokemon.id}">Release</button></li>`
-                    })
+            } else {
+                addPokemon(e)    
             }
-                
-            }
-        })
+        }
+    })
